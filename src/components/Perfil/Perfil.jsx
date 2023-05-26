11beb53251e6e9b98b607f8f30f77cfdc980245f
Jadir Acosta <jh.acosta@uniandes.edu.co>
2023-05-25 22:19:16 -0500

    Punto 1 OK

---
+++ src/components/Perfil/Perfil.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import style from './Perfil.module.css'
+
+export const Perfil = ({user}) =>{
+
+
+  return(
+  <div className={style['profile']}>
+  <div className={style['descripcion']}>
+    <img
+      src={user.avatar}
+      alt={user.tag}
+      className={style['avatar']}
+    />
+    <p className={style['name']}>{user.username}</p>
+    <p className={style['tag']}>{user.tag}</p>
+    <p className={style['location']}>{user.location}</p>
+  </div>
+
+  <ul className={style['stats']}>
+    <li>
+      <span className={style['label']}>Followers</span>
+      <span className={style['quantity']}>{user.stats.followers}</span>
+    </li>
+    <li>
+      <span className={style['label']}>Views</span>
+      <span className={style['quantity']}>{user.stats.views}</span>
+    </li>
+    <li>
+      <span className={style['label']}>Likes</span>
+      <span className={style['quantity']}>{user.stats.likes}</span>
+    </li>
+  </ul>
+</div>
+)
+
+}
+
+
+Perfil.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag:PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
+
